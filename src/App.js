6b81a6ccaa63399bc143e32ebcfcaf01817bc4f5
krista2728 <kristaskl27@gmail.com>
2023-03-11 23:46:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+const [places, setPlaces] = useState(data);
+const [showText, setShowText] = useState(false);
+
+  return (<div>
+
+<div className="container">
+<h1>{places.length} лучших турецких курортов </h1>
+  </div>
+
+
+  {places.map((item => {
+    const {id, placeName, description, image, source, showMore} = item;
+
+
+    const removePlace = (id) => {
+      let newPlaces = places.filter((place) => place.id  !== id)
+      setPlaces(newPlaces)
+    }
+
+    const showTextClick = (item) => {
+      item.showMore = !item.showMore
+      setShowText(!showText)
+    }
+
+    return( 
+      <div key={id}>
+
+        <div className="container">
+      <h2> {id} - {placeName} </h2>
+      </div>
+
+      <div className="container">
+      <p> {showMore ? description : description.substring(0, 220) + "...."} 
+      <button onClick={() => showTextClick(item)}>{showMore ? "См.меньше" : "См.больше"}</button>
+      
+      </p>
+      </div>
+
+      <div className="container">
+      <img src={image} width="500px" alt="plaj"/>
+      </div>
+
+      <div className="container">
+      <p> {source} </p>
+      </div>
+
+      <div className="container">
+    <button className="btn" onClick={() => removePlace(id)}>удалить</button>
+      </div>
+
+
+
+      </div>
+    )
+
+  }))}
+
+
+
+    
+    
     </div>
   );
 }
 
 export default App;
+
+
+
